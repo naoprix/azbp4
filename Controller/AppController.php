c50cb800077cba6adf47d0c5168f221298b644d5
naoprix <naoto.tohda@gmail.com>
2015-06-13 22:19:29 +0500

    DebugKit
    Debug Kit installed

--- Controller/AppController.php
+++ Controller/AppController.php
@@ -22,4 +22,43 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+
+	var $components = array(
+		'DebugKit.Toolbar',//DebugKitをアクティブにした。2015－06－09
+        'Session',
+		////
+		//CAKEPHP ブログチュートリアルより追加した。
+        'Auth' => array(
+            //'loginRedirect' => array(
+            //    'controller' => 'posts',
+            //    'action' => 'index'
+            //),
+            //'logoutRedirect' => array(
+            //    'controller' => 'pages',
+            //    'action' => 'display',
+            //    'home'
+            //),
+            'authenticate' => array(
+                'Form' => array(
+                    'passwordHasher' => 'Blowfish'
+                )
+            ),
+            //'authorize' => array('Controller') 
+        )
+    );
+
+    public function beforeFilter() {
+    	// どのページもindexとviewは閲覧できるらしい。
+        $this->Auth->allow('index', 'view');
+    }
+
+    /*
+    public function isAuthorized($user) {
+    if (isset($user['role']) && $user['role'] === 'admin') {
+        return true;
+    }
+    // デフォルトは拒否。Admin ユーザー以外は全員アクセスできない。
+    return false;
+    }*/
+    
 }
